730049e09e1052b5acd16c93c93c7dcbd427f81a
mollev545 <mollev545@hengersor.hu>
2025-03-06 12:56:15 +0100

    Táblázat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import './css/felveteli.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import { useState, useEffect } from 'react';
+import 'tachyons/css/tachyons.min.css';
 
 function App() {
   const [data, setData] = useState([]);
+  const [selectedAgazat, setSelectedAgazat] = useState("");
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/diakok")
@@ -21,22 +22,20 @@ function App() {
       .catch((error) => console.error("Hiba:", error));
   }, []);
 
+  const filteredData = selectedAgazat
+    ? data.filter((diak) => diak.agazat === selectedAgazat)
+    : data;
+
   return (
     <>
       <div className="bg-fej">
-        <div>
-          <img src="./img/oktatas-01.jpg" alt="Oktatás" />
-        </div>
+      <img src="./img/oktatas-01.jpg" alt="Oktatás" />
       </div>
 
       <div className="container-fluid row">
         <div className="bg-torzs col-md-4">
           <h3>Központi felvételi tájékoztató</h3>
-          <p className="text-left">
-            A középiskolákba történő jelentkezés során az iskolák határozzák meg, hogy a felvételi rangsort mi alapján döntik el.
-            A Jószakma Szakgimnázium a felvételi során az általános iskolából hozott és a központi felvételin szerzett pontok alapján
-            rangsorolja az iskolába jelentkezőket.
-          </p>
+          <p>A középiskolákba történő jelentkezés során az iskolák határozzák meg, hogy a felvételi rangsort mi alapján döntik el...</p>
           <a href="https://www.oktatas.hu/kozneveles/kozepfoku_felveteli_eljaras/kozepfoku_felveteli_eljaras_informacioi">
             Tájékoztató oldal
           </a>
@@ -46,12 +45,7 @@ function App() {
         <div className="col-md-4">
           <h1>Tájékoztatás</h1>
           <h3>Jószakma Szakgimnázium</h3>
-          <p>
-            A központi felvételit magyar nyelv és irodalom, illetve matematika tantárgyakból írják a jelentkezők. 
-            Mindkét tárgy esetén legfeljebb 50 pont szerezhető. A felvételiző hozott pontjait az általános iskolai év végi 
-            eredményei alapján számolják, ez a pontszám legfeljebb 50 pont lehet. A hozott pontokat duplázzák. A központi 
-            felvételin szerzett és a hozott pontok összege adja a felvételiző összesített pontszámát.
-          </p>
+          <p>A központi felvételit magyar nyelv és irodalom, illetve matematika tantárgyakból írják...</p>
           <img src="./img/e-mail-marketing-2745489__340.jpg" alt="Tájékoztatás" />
         </div>
 
@@ -59,12 +53,8 @@ function App() {
           <h3>Az oldal használatáról</h3>
           <h6>Ön az oldal használatával a következő információkhoz juthat hozzá:</h6>
           <ul>
-            <li>Előzetes rangsor:</li>
-            <ol>
-              <li>Nevek</li>
-              <li>Ágazat</li>
-              <li>Összes pontszám</li>
-            </ol>
+            <li>Előzetes rangsor</li>
+            <li>Nevek, Ágazat, Összes pontszám</li>
             <li>Előzetes rangsor nyelvi előkészítő</li>
             <li>A felvettek névsora</li>
           </ul>
@@ -73,8 +63,14 @@ function App() {
 
       <div className="row">
         <div className="col-md-5">
-          <h3 className="text-left">A felvételt nyert tanulók névsora a nyelvi előkészítőre</h3>
-          <div></div>
+          <h3>A felvételt nyert tanulók névsora a nyelvi előkészítőre</h3>
+          <label>Válassz egy ágazatot:</label>
+          <select onChange={(e) => setSelectedAgazat(e.target.value)}>
+            <option value="">Összes</option>
+            <option value="informatika">Informatika</option>
+            <option value="elektronika">Elektronika</option>
+          </select>
+          <button onClick={() => setSelectedAgazat("")}>Adatok</button>
         </div>
       </div>
 
@@ -87,12 +83,13 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {data.length > 0 ? (
-            data.map((diak, index) => (
+          {filteredData.length > 0 ? (
+            filteredData.map((diak, index) => (
               <tr key={index}>
-                <td>{diak.nev}</td>
-                <td>{diak.agazat}</td>
-                <td>{diak.osszpont}</td>
+                
+                <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">{diak.nev}</th>
+                <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">{diak.agazat}</th>
+                <th class="fw6 bb b--black-20 tl pb3 pr3 bg-white">{diak.osszpont}</th>
               </tr>
             ))
           ) : (
